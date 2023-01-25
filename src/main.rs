8f3d3e85063e9591bc3b4b7a3866b6cbf6b23a74
gkotecki <gabrielkotecki@gmail.com>
2023-01-24 23:12:29 -0300

    ADD: day01 data and output template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let day = "01";
+    println!("\n---------------- Running day {day} challenge ----------------");
+
+    let file_path = format!("src/day_{day}/input.txt");
+    println!("Reading file path: {file_path}");
+
+    let file_contents = "123123";
+    println!("\nParsed file contents:\n{file_contents}");
+
+    println!("\nPart 1 response:\n{file_contents}");
+    println!("\nPart 2 response:\n{file_contents}");
+    println!("----------------------------------------------------------");
+    println!("\n\n\tAll done!\n\n");
 }
